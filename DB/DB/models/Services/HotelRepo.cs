b6b29eb1d97e96b9cb9e5ec3eb42540ed94cd53d
Lesley Rivera <les.aguilar@live.com>
2020-07-22 14:14:27 -0700

    added hotel interface

---
+++ DB/DB/models/Services/HotelRepo.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DB.Interfaces;
+using DB.Properties.Data;
+using DB.Properties.models;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DB.models.Services
+{
+    public class HotelRepo : IHotel
+    {
+
+        private AsyncInnDbContext _context;
+
+        public HotelRepo(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Hotel> Create(Hotel hotel)
+        {
+            _context.Entry(hotel).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            await _context.SaveChangesAsync();
+
+            return hotel;
+        }
+
+        public async Task Delete(int id)
+        {
+            Hotel hotel = await GetHotel(id);
+            _context.Entry(hotel).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Hotel> GetHotel(int id)
+        {
+            Hotel hotel = await _context.Hotels.FindAsync(id);
+            return hotel;
+        }
+
+        public async Task<List<Hotel>> GetHotels()
+        {
+           var hotels = await _context.Hotels.ToListAsync();
+            return hotels;
+        }
+
+        public async Task<Hotel> Update(Hotel hotel)
+        {
+            _context.Entry(hotel).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return hotel;
+        }
+            
+        }
+    }
+
+
