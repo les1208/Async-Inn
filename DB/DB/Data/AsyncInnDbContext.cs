4ced8cb03884b9b4a06b9c9f8251d630daf6a353
Lesley Rivera <les.aguilar@live.com>
2020-07-21 18:26:59 -0700

    Lab 12 Completed

---
+++ DB/DB/Data/AsyncInnDbContext.cs
@@ -0,0 +1,103 @@
+﻿using DB.Properties.models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DB.Properties.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Hotel>().HasData(
+             new Hotel
+             {
+                 Id = 1,
+                 Name = "Hale Koa",
+                 Address = "2055 Kalia Rd",
+                 City = "Honolulu",
+                 State = "Hawaii",
+                 PhoneNumber = "+1 (808) 955-0555"
+             },
+
+             new Hotel
+             {
+                 Id = 2,
+                 Name = "The Royal Hawaiian",
+                 Address = "2259 Kalakaua",
+                 City = "Honolulu",
+                 State = "Hawaii",
+                 PhoneNumber = "+1 (808) 923-7311"
+             },
+
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Hilton Hawaiian Village",
+                    Address = "2005 Kalia Rd",
+                    City = "Honolulu",
+                    State = "Hawaii",
+                    PhoneNumber = "+1 (808) 949-4321"
+                });
+
+            modelBuilder.Entity<Room>().HasData(
+                new Room
+                {
+                    Id = 1,
+                    Name = "Honu Suite",
+                    Layout = "1 bedroom"
+                },
+
+
+                new Room
+                {
+                    Id = 2,
+                    Name = "Aloha Suite",
+                    Layout = "2 bedrooms"
+                },
+
+
+                new Room
+                {
+                    Id = 3,
+                    Name = "King Kamekameha",
+                    Layout = "3 bedrooms"
+                });
+
+            modelBuilder.Entity<Amenity>().HasData(
+                new Amenity
+                {
+                    Id = 1,
+                    Name = "Mini Bar",
+                },
+
+                new Amenity
+                {
+                    Id = 2,
+                    Name = "Free use of beach rentals!"
+                },
+
+                new Amenity
+                {
+                    Id = 3,
+                    Name = "Bottomless Mimosas"
+                });
+        }
+
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<Amenity> Amenities { get; set; }
+    }
+
+    }
---
+++ DB/DB/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using DB.Properties.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DB
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        //the appsettings.json, by default, is our "Configurations" for the app.
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+  
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is where all dependecies are going to live
+            //Enable the use of using controllers withing the MVC convention
+            services.AddControllers();
+
+            //Register with the app, that the db exists, and what options to use for it.
+            services.AddDbContext<AsyncInnDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {   //set out default routing for our request within the API  app
+                //By default, our convention is {site}/[controller]/[action]/[id]
+                //id is not requiered, allowing it to be nullable
+                endpoints.MapControllerRoute("default", "{Controller=Home}/{action=Index}/{id?}");
+                
+            });
+        }
+    }
+}
